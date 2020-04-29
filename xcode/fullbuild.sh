43b333a46ceff73e936ba8ccbe57a784787c4e5e
Eben Rauhut <erauhut@humatics.com>
2020-04-29 16:10:02 -0400

    Disable iOS build

--- xcode/fullbuild.sh
+++ xcode/fullbuild.sh
@@ -7,8 +7,11 @@ CINDER_XCODEPROJ="${CURRENT_DIR}/cinder.xcodeproj"
 xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder -configuration Release $@
 xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder -configuration Debug $@
 
-# iOS
-xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone -configuration Release -sdk iphoneos $@
-xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone -configuration Debug -sdk iphoneos $@
-xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone_sim -configuration Release -sdk iphonesimulator $@
-xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone_sim -configuration Debug -sdk iphonesimulator $@
+# humatics - erauhut 29 apr 2020 - disable iOS builds as these are unused currently and
+# would have to be retargeted in order to work properly
+
+## iOS
+#xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone -configuration Release -sdk iphoneos $@
+#xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone -configuration Debug -sdk iphoneos $@
+#xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone_sim -configuration Release -sdk iphonesimulator $@
+#xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder_iphone_sim -configuration Debug -sdk iphonesimulator $@
